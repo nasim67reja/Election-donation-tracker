98985bde7f08fbed224ddf030c68a851f98f2a5c
nasim67reja <nasimreja67@gmail.com>
2023-10-31 11:38:18 +0600

    testing font family

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,11 +1,11 @@
-import { Raleway, Roboto_Mono } from 'next/font/google'
+import { Raleway, Lora } from 'next/font/google'
  
 export const raleway = Raleway({
   subsets: ['latin'],
   display: 'swap',
 })
  
-export const roboto_mono = Roboto_Mono({
+export const lora = Lora({
   subsets: ['latin'],
   display: 'swap',
 })
--- components/specific/Hero.tsx
+++ components/specific/Hero.tsx
@@ -2,18 +2,22 @@ import React from "react";
 import SectionLayout from "../common/SectionLayout";
 import Link from "next/link";
 import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+import { lora } from "@/app/fonts";
 
 const Hero = () => {
   return (
     <div className="backG min-h-[60vh] xl:min-h-[90vh] flex justify-center items-center">
       <SectionLayout bg="">
         <div className=" flex flex-col justify-center items-center">
-          <h1 data-aos="zoom-in" className="heading-primary text-[#fff] lora ">
+          <h1
+            data-aos="zoom-in"
+            className={`heading-primary text-[#fff] ${lora.className}`}
+          >
             Empower Your Campaign with <br /> Shadhin Lab LLC
           </h1>
           <p
             data-aos="fade-up"
-            className="text-[16px] md:text-[18px] 2xl:text-[22px] text-center !text-[#fff] lora"
+            className={`text-[16px] md:text-[18px] 2xl:text-[22px] text-center !text-[#fff] ${lora.className}`}
           >
             A dedicated platform for election candidates to fund their campaigns
             and make a lasting impact.
@@ -21,10 +25,12 @@ const Hero = () => {
           <button data-aos="fade-up" className="mt-[4rem]">
             <Link
               href="#about"
-              className="rounded-[5px] py-3 px-8 lg:py-6 gap-6 lg:px-12 !text-[#fff] text-[14px] border border-whtie duration-200 hover:bg-primary   hover:border-primary flex justify-between items-center  font-bold"
+              className="rounded-[5px] py-3 px-8 lg:py-6 gap-4 lg:px-12 !text-[#fff] text-[14px] border border-whtie duration-200 hover:bg-primary   hover:border-primary flex justify-between items-center  font-bold"
             >
-              <span>Learn More</span>
-              <FiChevronDown className="h-[24px] w-[24px] animate-bounce" />
+              <span className={lora.className}>Learn More</span>
+              <span className="translate-y-1">
+                <FiChevronDown className="h-[24px] w-[24px] animate-bounce " />
+              </span>
             </Link>
           </button>
         </div>
